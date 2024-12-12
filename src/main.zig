f1f4d904d30a4bce31713319d3b0076da94c7eae
87flowers <87flowers@noreply.codeberg.org>
2024-12-12 21:43:33 +0000

    0.20

--- src/main.zig
+++ src/main.zig
@@ -151,7 +151,7 @@ const Uci = struct {
             g.reset();
         } else if (std.mem.eql(u8, command, "uci")) {
             try self.output.print("{s}\n", .{
-                \\id name Bannou 0.19
+                \\id name Bannou 0.20
                 \\id author 87 (87flowers.com)
                 \\uciok
             });
